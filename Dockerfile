3d327d1809b04935fc7ac74f7768ad0ab1d45f51
jsandas <jsandas@users.noreply.github.com>
2022-05-30 17:34:25 -0600

    build images for each version

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,43 @@
-FROM python:slim
+# get mcutils binary
+FROM golang as mcutils
+
+RUN go install github.com/xrjr/mcutils/cmd/mcutils@latest
+
+# get minecraft bedrock server
+FROM debian:bullseye as build
+
+ARG MC_VER=1.19.10.20
+
+ENV DEBIAN_FRONTEND=noninteractive
+ENV MINECRAFT_VER=${MC_VER}
+
+WORKDIR /build
+
+RUN apt update && apt upgrade -y \
+    && apt install -y curl unzip
+
+RUN curl -O https://minecraft.azureedge.net/bin-linux/bedrock-server-${MINECRAFT_VER}.zip \
+    && unzip -qq bedrock-server-${MINECRAFT_VER}.zip \
+    && rm bedrock-server-${MINECRAFT_VER}.zip \
+    && echo ${MINECRAFT_VER} > /build/version
+
+# put it all together
+FROM debian:bullseye
 
 ENV DEBIAN_FRONTEND=noninteractive
 ENV APP_DIR=/opt/minecraft
 
-RUN useradd -d ${APP_DIR} -s /bin/bash minecraft 
-
 RUN apt update && apt upgrade -y \
-    && apt install -y curl unzip \
+    && apt autoremove -y \
     && rm -rf /var/lib/apt/lists/*
 
-RUN pip install mcstatus
+RUN useradd -m -d ${APP_DIR} -s /bin/bash minecraft \
+    && chown -R minecraft ${APP_DIR}
 
 WORKDIR ${APP_DIR}
+
+COPY --from=mcutils /go/bin/mcutils /usr/local/bin/mcutils
+COPY --from=build /build ${APP_DIR}
 
 COPY docker-entrypoint.sh ${APP_DIR}/docker-entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
 
 APP_DIR=${APP_DIR:-/opt/minecraft}
-MINECRAFT_VER=$(curl -s -H "accept-language:*" -H "User-Agent: Mozilla/5.0" https://www.minecraft.net/en-us/download/server/bedrock | grep serverBedrockLinux | grep -o -P '(?<=bedrock-server-).*(?=.zip)')
-DOWNLOAD_URL="https://minecraft.azureedge.net/bin-linux/bedrock-server-$MINECRAFT_VER.zip"
 
 function config() {
     _envvars=$(env | egrep '^CFG_' | sed 's/CFG_//g' | sed 's/_/-/g')
     for _envvar in ${_envvars[@]}
     do
-        _key=$(echo $_envvar | awk -F '=' '{print $1}' | tr '[:upper:]' '[:lower:]') 
-        _val=$(echo $_envvar | awk -F '=' '{print $2}')
+        _key=$(echo ${_envvar} | awk -F '=' '{print $1}' | tr '[:upper:]' '[:lower:]') 
+        _val=$(echo ${_envvar} | awk -F '=' '{print $2}')
 
         echo " Updating $_key..."
-        sed -i 's/'"${_key}"'=.*/'"${_key}"'='"${_val}"'/g' $APP_DIR/server.properties
+        sed -i 's/'"${_key}"'=.*/'"${_key}"'='"${_val}"'/g' ${APP_DIR}/server.properties
     done
 }
 
-if [[ ! -d $MINECRAFT_VER ]]; then
-    echo "Installing minecraft version: $MINECRAFT_VER"
-
-    # Download and decompress archive
-    curl -O $DOWNLOAD_URL
-    unzip -qq bedrock-server-$MINECRAFT_VER.zip
-    rm bedrock-server-$MINECRAFT_VER.zip
-fi
-
 config
 
-echo "Starting minecraft server..."
+echo "Starting minecraft server version: $(cat $APP_DIR/version)..."
 LD_LIBRARY_PATH=. ./bedrock_server
