e6518e6009c023bc458961274c8ad94b736a7710
jsandas <jsandas@users.noreply.github.com>
2022-06-13 21:28:11 -0600

    check if version is empty

--- version_check.sh
+++ version_check.sh
@@ -4,16 +4,18 @@ MC_VER=$(curl -s -H "accept-language:*" -H "User-Agent: Mozilla/5.0" https://www
 CUR_VER=$(docker run ghcr.io/jsandas/minecraft-bedrock:latest cat version)
 
 
-if [[ "{{ env.MC_VER }}" != "{{ env.CUR_VER }}" ]]; then 
-    sed -i 's/MC_VER=.*/MC_VER={{ env.MC_VER }}/g' Dockerfile
+if [[ "$MC_VER" != "$CUR_VER" && "$MC_VER" != "" ]]; then 
+    sed -i 's/MC_VER=.*/MC_VER='"$MC_VER"'/g' Dockerfile
 fi
       
 git diff --no-ext-diff --quiet --exit-code
 if [[ $? -eq 1 ]]; then
-git config --global user.name "jsandas"
-git config --global user.email "jsandas@users.noreply.github.com"
+    echo "changes detected"
+    echo $MC_VER $CUR_VER
+    # git config --global user.name "jsandas"
+    # git config --global user.email "jsandas@users.noreply.github.com"
 
-git add -A
-git commit -m "update mc version to {{ env.MC_VER}}"
-git push
+    # git add -A
+    # git commit -m "update mc version to $MC_VER}}"
+    # git push
 fi
