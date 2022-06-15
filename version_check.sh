78e144c66c7998e3dbbfe2f2bc9e73d9f94cab79
jsandas <jsandas@users.noreply.github.com>
2022-06-14 18:58:11 -0600

    decrease frequency and enable commit

--- version_check.sh
+++ version_check.sh
@@ -5,17 +5,18 @@ CUR_VER=$(docker run ghcr.io/jsandas/minecraft-bedrock:latest cat version)
 
 
 if [[ "$MC_VER" != "$CUR_VER" && "$MC_VER" != "" ]]; then 
+    echo " New version found: $MC_VER"
     sed -i 's/MC_VER=.*/MC_VER='"$MC_VER"'/g' Dockerfile
 fi
       
 git diff --no-ext-diff --quiet --exit-code
 if [[ $? -eq 1 ]]; then
-    echo "changes detected"
+    echo " Commiting updated Dockefile..."
     echo $MC_VER $CUR_VER
-    # git config --global user.name "jsandas"
-    # git config --global user.email "jsandas@users.noreply.github.com"
+    git config --global user.name "jsandas"
+    git config --global user.email "jsandas@users.noreply.github.com"
 
-    # git add -A
-    # git commit -m "update mc version to $MC_VER}}"
-    # git push
+    git add -A
+    git commit -m "update mc version to $MC_VER}}"
+    git push
 fi
