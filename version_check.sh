1854c3a0d45b934570cf0017390e46a3b757756c
jsandas <jsandas@users.noreply.github.com>
2023-06-04 15:41:11 -0600

    fix version_check script

--- version_check.sh
+++ version_check.sh
@@ -2,7 +2,7 @@
 
 MC_VER=$(curl -s -H "accept-language:*" -H "User-Agent: Mozilla/5.0" https://www.minecraft.net/en-us/download/server/bedrock | grep serverBedrockLinux | grep -o -P '(?<=bedrock-server-).*(?=.zip)')
 IMAGE_VER=$(docker run ghcr.io/jsandas/minecraft-bedrock:latest cat version)
-CHART_VER=$(cat chart/minecraft-bedrock/Chart.yaml | grep appVersion | awk '{print $2}' | tr -d '"')
+CHART_VER=$(cat charts/minecraft-bedrock/Chart.yaml | grep appVersion | awk '{print $2}' | tr -d '"')
 
 if [[ -z $MC_VER || -z $IMAGE_VER || -z $CHART_VER ]]; then
     echo " Failed to retrieve all versions"
@@ -13,7 +13,7 @@ fi
 if [[ "$MC_VER" != "$IMAGE_VER" || "$MC_VER" != "$CHART_VER" ]]; then 
     echo " New version found: $MC_VER"
     sed -i 's/MC_VER=.*/MC_VER='"$MC_VER"'/g' Dockerfile
-    sed -i 's/^appVersion:.*/appVersion: \"'"$MC_VER"'\"/g' chart/minecraft-bedrock/Chart.yaml
+    sed -i 's/^appVersion:.*/appVersion: \"'"$MC_VER"'\"/g' charts/minecraft-bedrock/Chart.yaml
 fi
       
 # git diff --no-ext-diff --quiet --exit-code
